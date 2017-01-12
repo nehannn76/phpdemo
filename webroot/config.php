ac9f58ede2af6f44e38ae2551c95734fad681833
neha.gupta <neha.gupta@atos.net>
2017-01-12 12:20:17 +0530

    updated

--- webroot/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', '172.30.61.191');
-   define('DB_USERNAME', 'admin');
-     define('DB_PASSWORD', 'root');
-   define('DB_DATABASE','test');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) ;
-?>
